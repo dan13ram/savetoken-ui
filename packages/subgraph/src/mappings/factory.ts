40321c83b10c05b2d3ea7bee97411fa350d2d028
dan13ram <dan13ram@gmail.com>
2021-06-09 22:48:31 +0530

    init subgraph

---
+++ packages/subgraph/src/mappings/factory.ts
@@ -0,0 +1,12 @@
+import { SaveToken } from "../types/schema";
+import { SaveTokenCreated as SaveTokenCreatedEvent } from "../types/SaveTokenFactory/SaveTokenFactoryABI";
+
+export function handleSaveTokenCreated(event: SaveTokenCreatedEvent): void {
+    let txHash = event.transaction.hash;
+    let address = event.params.addr;
+    let saveToken = new SaveToken(address.toHexString());
+    saveToken.address = address;
+    saveToken.creationTx = txHash;
+    saveToken.factoryAddress = event.address;
+    saveToken.save();
+}
